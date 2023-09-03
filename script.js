266b996ea8998ddaa3c957c64a0e4251f4fa85f1
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-09-03 10:23:31 +0530

    Working with Arrays

---
+++ script.js
@@ -0,0 +1,21 @@
+// Declare a variable
+var greeting ="Hello, Everyone!";
+
+// Define a function
+function sayHello() {
+    console.log(greeting);
+}
+
+// Call a function
+sayHello();
+
+// create an array
+var fruits = ["apple", "orange", "Banana"];
+
+// Access elements in the array
+console.log(fruits[0]);
+
+// Add an element to the end of the array
+fruits.push("cherry");
+console.log(fruits);
+
