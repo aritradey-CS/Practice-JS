eaddf28536ba269feb0e5d0ee0d6ffb3c00c72e3
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-09-04 12:25:36 +0530

    n is a var,it crease it's value everytime it prints

--- Leetcode 2/day challenge/script.js
+++ Leetcode 2/day challenge/script.js
@@ -7,3 +7,18 @@ const sayHello = createHelloWorld();
 
 // Calling the new function
 console.log(sayHello());    // Output: "Hello word"
+
+//-------------------------------------------------------------------------------------------------
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+function createCounter(n) {
+    return () => n++;
+}
+
+//Create a counter starting from 5
+const myCounter = createCounter(5);
+
+// Call the counter function multiple times to see the values increment
+console.log(myCounter());   //Output: 5
+console.log(myCounter());   //Output: 6
+console.log(myCounter());   //Output: 7
