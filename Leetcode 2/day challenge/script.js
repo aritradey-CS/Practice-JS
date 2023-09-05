c63b065eed31c4f760619b107bf41d831aa302ea
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-09-05 19:52:15 +0530

    javascript true/not true concept of object val using func

--- Leetcode 2/day challenge/script.js
+++ Leetcode 2/day challenge/script.js
@@ -22,3 +22,31 @@ const myCounter = createCounter(5);
 console.log(myCounter());   //Output: 5
 console.log(myCounter());   //Output: 6
 console.log(myCounter());   //Output: 7
+
+
+
+//-------------------------------------------------------------------------------------------------
+//Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+//toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+//notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+
+function expect(value) {
+    return {
+        toBe: function (expected){
+            return value === expected;
+        },
+        notToBe: function(expected){
+            return value !== expected;
+        }
+    };
+}
+
+// Example usage:
+
+const result1 = expect(5).toBe(5);      //true because 5 === 5
+const result2 = expect("hello").notToBe("world");   //true because "hello" !== "world"
+const result3 = expect(5).toBe(null);      //true because 5 === 5
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
