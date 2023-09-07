ab2df65b9ad0a9c18117a2f2ab912456d15dc8c8
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-09-07 12:10:33 +0530

    do-while (testing)

--- script.js
+++ script.js
@@ -91,3 +91,13 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 numbers.forEach((number) => {
     console.log(number);
 });
+
+// ________________________________________________________________
+// Using do-while in JavaScript
+
+let count = 0;
+
+do {
+    console.log("This will run at least onces");
+    count++;
+} while (count<3);
